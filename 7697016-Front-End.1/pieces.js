0973b1e8ca4d59ad3294cbff251d7a5b60bf5479
pierre <jobrider@gmail.com>
2023-06-13 10:46:24 +0200

    ajout bouton tri et filtrage et range pour filtrer le prix, les produits sont modifiés en temps reel

--- 7697016-Front-End.1/pieces.js
+++ 7697016-Front-End.1/pieces.js
@@ -6,69 +6,141 @@ const params = new URLSearchParams(url.search)
 const cat = params.get('cat')
 
 
+//Premier affichage de la page : appel de la fonction ajouter FichePoduit via la variable cat passé via index.html
+pieces.forEach((piece) => {
+    if (piece.categorie === cat) {
+        ajouterFicheProduit(piece);
+    }
+});
+
+//Afficher le titre de la vategorie envoyé depuis la page index
+const titreCategorie = document.querySelector('.titre-categorie')
+titreCategorie.innerHTML = `Produits de la catégorie : <b>${cat} </b>` //console.log(titreCategorie.innerhtml)
+
+// TRIER // Bouton Trier
 const btTrier = document.querySelector('.btn-trier')
 btTrier.addEventListener('click', () => {
 
-    const produitsCategorie = pieces.filter((piece) => piece.categorie === cat);
+    document.querySelector('.fiches').innerHTML = ""//vider tout dans .fiches
+
+    const produitsCategorie = pieces.filter((piece) => piece.categorie === cat)
 
     //console.log(produitsCategorie)
     
     const produitsTries = Array.from(produitsCategorie).sort(function (a, b) {
         return a.prix - b.prix
     })
-    console.log(produitsTries)
-}) 
+    //console.log(produitsTries)
 
-
-const btFiltrer = document.querySelector('.btn-filtrer')
-btFiltrer.addEventListener('click', () => {
-
-    //Mettre dans un nouveau tableau les produits de la cat 
-    const produitsCategorie = pieces.filter((piece) => piece.categorie === cat);
-
-    //console.log(produitsCategorie)
-    
-    const produitsFiltres = Array.from(produitsCategorie).filter(function (piece) {
-        return piece.prix < 35
+    //aficher titre
+    produitsTries.forEach(piece => { //apeler fonction avec tableau de data
+        ajouterFicheProduit(piece)
     })
-    console.log(produitsFiltres)
+    
+    document.querySelector('.titre-categorie').innerText = "Produits du - chers au + chers"
 }) 
 
+// FILTRER // Bouton Filtrer
+const boutonFiltrer = document.querySelector(".btn-filtrer")
 
-const titreCategorie = document.createElement('h2')
-titreCategorie.innerHTML = `Produits de la catégorie : <b>${cat}</b>` //console.log(titreCategorie.innerhtml)
-const fichesElement2 = document.querySelector('.fiches')
-fichesElement2.appendChild(titreCategorie)
+boutonFiltrer.addEventListener("click", function () {
 
-pieces.forEach((piece) => {
-    if (piece.categorie === cat) {
+    // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+    document.querySelector(".fiches").innerHTML = ""
 
-      const fichesElement = document.querySelector('.fiches')
-  
-      const divFiche = document.createElement('div')
-      divFiche.classList.add('card')
-  
-      const imageElement = document.createElement('img')
-      imageElement.src =   piece.image === null || piece.image === undefined || piece.image === ""
-      ? 'images/no-picture.jpg' : piece.image
-  
-      const titreElement = document.createElement('h2')
-      titreElement.innerText = piece.nom
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix < 35
+    });
 
-        const prixElement = document.createElement('p')
-        prixElement.innerText = `Prix : ${piece.prix}€`
-        
-        const indicateurPrix = document.createElement('p')
-        indicateurPrix.innerText = `Indicateur prix : ${piece.prix < 35 ? "€€" : "€€€"}`
+    piecesFiltrees.forEach(piece => {
+        ajouterFicheProduit(piece)
+    })
 
-        const descriptionElement = document.createElement('p')
-        descriptionElement.innerText = piece.description ?? "Pas de description disponible."
+    document.querySelector('.titre-categorie').innerText = "Produits abordables (-35€)"
+})
+
+//INPUT RANGE // Bouton pour afficher produits dont le prix = prix du input range
+const priceRange = document.getElementById('price-range')
+const btRange = document.querySelector('input[type=range]')
+
+btRange.addEventListener('input', (event) => {
     
-        const categorieElement = document.createElement('p')
-        categorieElement.innerText = piece.categorie ?? ("pas de catégorie")
-        //console.log(titreElement.innerText)
+    //Affiche le prix du range
+    const value = event.target.value
+    priceRange.innerText = `${value}€`
 
-        
+    //
+    const produitsCategorie = pieces.filter((piece) => piece.categorie === cat)
+    
+    const prixMax = produitsCategorie.filter(function (piece) { return piece.prix <= value })
+
+    document.querySelector(".fiches").innerHTML = ""
+    prixMax.forEach(piece => {
+        ajouterFicheProduit(piece)
+    })
+
+})
+
+
+
+//Afficher une liste ul li des produits abordable (soit < 35€)
+
+//Methode map
+/* const nomproduitsAbordables = pieces.map(piece => piece.nom)
+const descriptionproduitsAbordables = pieces.map( piece => piece.description)
+
+//Methode splice pour filtrer les produits 
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        nomproduitsAbordables.splice(i, 1)
+        descriptionproduitsAbordables.splice(i,1)
+    }
+}
+//console.log(nomproduitsAbordables + descriptionproduitsAbordables)
+
+ 
+const ulElement = document.createElement('ul')
+ 
+ for (let i = 0; i < nomproduitsAbordables.length; i++){
+     const liElement = document.createElement('li')
+     liElement.innerText = `${nomproduitsAbordables[i]} - ${descriptionproduitsAbordables[i]}`
+     ulElement.appendChild(liElement)
+ }
+ document.querySelector('.abordable').appendChild(ulElement)
+ //console.log(noms) */
+
+/////////////////////////////////////////////////////////////////////////////
+//Fonction qui affiche tous ls produits selon le tableau qu'on lui donne
+function ajouterFicheProduit(piece) {
+
+
+    //console.log('dans la fonction', piece)
+
+    const fichesElement = document.querySelector('.fiches')
+
+    const divFiche = document.createElement('div')
+    divFiche.classList.add('card')
+
+    const imageElement = document.createElement('img')
+    imageElement.src =   piece.image === null || piece.image === undefined || piece.image === ""
+        ? 'images/no-picture.jpg' : piece.image    
+    console.log(imageElement)
+
+    const titreElement = document.createElement('h2')
+    titreElement.innerText = piece.nom
+
+    const prixElement = document.createElement('p')
+    prixElement.innerText = `Prix : ${piece.prix}€`
+
+    const indicateurPrix = document.createElement('p')
+    indicateurPrix.innerText = `Indicateur prix : ${piece.prix < 35 ? "€€" : "€€€"}`
+
+    const descriptionElement = document.createElement('p')
+    descriptionElement.innerText = piece.description ?? "Pas de description disponible."
+
+    const categorieElement = document.createElement('p')
+    categorieElement.innerText = piece.categorie ?? ("pas de catégorie")
+    //console.log(titreElement.innerText)        
 
         divFiche.appendChild(imageElement)
         divFiche.appendChild(titreElement)
@@ -77,6 +149,6 @@ pieces.forEach((piece) => {
 
         fichesElement.appendChild(divFiche)
 
-    }
-});
+}
+
 
