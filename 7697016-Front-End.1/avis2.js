75e533f55405be73cc7dabe1838fad55eee6662b
pierre <jobrider@gmail.com>
2023-06-15 12:02:58 +0200

    page avis.html fonctionne avec fetch

--- 7697016-Front-End.1/avis2.js
+++ 7697016-Front-End.1/avis2.js
@@ -13,37 +13,27 @@ pieces.forEach((piece, index) => {
     liElement2.innerText = `${piece.nom} - Avis`
 
     ulElement2.appendChild(liElement2)
-        
-    liElement2.addEventListener("click", async function (event) {
 
-        const id = liElement2.getAttribute('id')
-        console.log('id du li : ', id)
- 
-        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis")
-        
-        const avis = await reponse.json()
+    //AddeventListener avec MOUSEOVER
+    liElement2.addEventListener('click', async function (event) {
+    
+    const id = liElement2.getAttribute('id')
+    //console.log('id du li : ', id)
+    
+    //Fetch
+    const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis")        
+    const avis = await reponse.json()
+
+    //Insérer tous les avid pour un produit
+    for (let i = 0; i < avis.length; i++){
+        const pAvis = document.createElement('p')
+
+        pAvis.innerHTML = '<b>' + avis[i].utilisateur + ' : </b>' + avis[i].commentaire
+
+        liElement2.appendChild(pAvis)
+    }
+    
     })
-
-
-
-/*     piecesElements[i].addEventListener("click", async function (event) {
-           
-        const id = event.target.dataset.id;
-
-        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis")
-        
-        const avis = await reponse.json()
-        
-        const pieceElement = event.target.parentElement;
-
-        const avisElement = document.createElement("p");
-
-        for (let i = 0; i < avis.length; i++) {
-            avisElement.innerHTML += `<b>${avis[i].utilisateur}: </b>${avis[i].commentaire} <br>--<br>`;
-            console.log('avis num : ', avis[i])
-        }
-        pieceElement.appendChild(avisElement)
-    }); */
 })
 
 fichesElement.appendChild(ulElement2) 
